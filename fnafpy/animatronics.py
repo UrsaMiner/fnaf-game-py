e37261bf27a585f6d1dde3a6398305c5b1aace68
VulpusGenius <69985070+UrsaMiner@users.noreply.github.com>
2024-03-01 14:30:23 +0000

    Create animatronics.py
    for everything animatronic related

---
+++ fnafpy/animatronics.py
@@ -0,0 +1,8 @@
+# for everything animatronic
+class robot(): # robot class
+    def __init__(self,route,speed,name): # initialization
+        # functional
+        self.route = route # list
+        self.speed = speed # value from 1 to 20
+        # cosmetic
+        self.name = name
